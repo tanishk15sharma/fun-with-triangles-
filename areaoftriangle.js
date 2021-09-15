56e133eb55d06d092c70f9bfb26ca8accd63c833
tanishk15sharma <tanishk1582002@gmail.com>
2021-09-15 21:49:00 +0530

    find-area
    last part  (find area of triangle ) has done using  HTML and JS and ready to use !

---
+++ areaoftriangle.js
@@ -0,0 +1,26 @@
+const sides = document.querySelectorAll(".side-input")
+const areaBtn = document.querySelector("#area-btn")
+const outPut = document.querySelector("#output")
+
+function calculateBaseIntoHeight(base, height) {
+    const baseIntoHeight = (base * height) * 1 / 2;
+    return baseIntoHeight;
+
+
+
+}
+
+function calculateArea() {
+    const baseIntoHeight = calculateBaseIntoHeight(Number(sides[0].value), Number(sides[1].value));
+
+    outPut.innerText = "The Area of Triangle  " + baseIntoHeight
+
+
+
+    //console.log(baseIntoHeight);
+
+
+}
+
+
+areaBtn.addEventListener("click", calculateArea);
