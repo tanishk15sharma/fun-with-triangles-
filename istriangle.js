82dac5663022aee376be5634b749785e3f8ab026
tanishk15sharma <tanishk1582002@gmail.com>
2021-09-26 14:51:00 +0530

    feat: -value and empty msg
    in is triangle new feature is added ; -value and empty field when user click

--- istriangle.js
+++ istriangle.js
@@ -4,26 +4,60 @@ var Angle3 = document.querySelector("#angle3")
 var isTriangleBtn = document.querySelector("#is-triangle-btn")
 var outPut = document.querySelector("#output")
 
-function calculateSumOfAngles (angle1,angle2,angle3) {
-    
-    const  sumOfAngles = angle1+angle2+angle3
-    return sumOfAngles;
-         }
 
-    function isTriangle () {
-        const sumOfAngles = calculateSumOfAngles(Number(Angle1.value),Number(Angle2.value),Number(Angle3.value))
-        
-        if(sumOfAngles===180) {
-            outPut.innerHTML = "YAP ! The angles form a Triangle "
+isTriangleBtn.addEventListener("click", checkButtonHandler);
+
+
+function checkButtonHandler() {
+
+    var angle1 = Number(Angle1.value);
+    var angle2 = Number(Angle2.value);
+    var angle3 = Number(Angle3.value);
+
+
+    if ((angle1 !== "") && (angle2 !== "") && (angle3 !== "")) {
+
+        if ((Math.sign(angle1) === 1) && (Math.sign(angle2) === 1) && (Math.sign(angle3) === 1)) {
+            outPut.innerText = "";
+            isTriangle();
+        } else if ((Math.sign(angle1) === -1) || (Math.sign(angle2) === -1) || (Math.sign(angle3) === -1)) {
+            showOutputText("Angles can't be negatives. Please enter  + value.");
+        } else if ((Math.sign(angle1) === 0) || (Math.sign(angle2) === 0) || (Math.sign(angle3) === 0)) {
+            showOutputText("Angles can't be empty. ");
         }
-
-        else {
-            outPut.innerHTML = "oh no ! The angles don't form a triangle "
-        }
-
 
     }
-    
+}
 
-    isTriangleBtn.addEventListener("click",isTriangle)
 
+function calculateSumOfAngles(angle1, angle2, angle3) {
+
+    const sumOfAngles = angle1 + angle2 + angle3
+    return sumOfAngles;
+
+}
+
+
+
+function isTriangle() {
+    const sumOfAngles = calculateSumOfAngles(Number(Angle1.value), Number(Angle2.value), Number(Angle3.value));
+
+
+
+    if (sumOfAngles === 180) {
+        outPut.innerText = "YAP ! The angles form a Triangle "
+
+    } else {
+        outPut.innerText = "oh no ! The angles don't form a triangle "
+    }
+
+
+}
+
+function showOutputText(msg) {
+
+    outPut.innerText = msg;
+}
+
+
+//isTriangleBtn.addEventListener("click",isTriangle)
