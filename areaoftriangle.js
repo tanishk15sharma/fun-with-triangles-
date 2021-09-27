5a4b667f5a79a298773cbda5a27380bb7d4f57bd
tanishk15sharma <tanishk1582002@gmail.com>
2021-09-27 21:42:13 +0530

    fix
    added output for empty  || -value

--- areaoftriangle.js
+++ areaoftriangle.js
@@ -3,16 +3,31 @@ const areaBtn = document.querySelector("#area-btn")
 const outPut = document.querySelector("#output")
 
 function calculateBaseIntoHeight(base, height) {
-    const baseIntoHeight = (base * height) * 1 / 2;
+    const baseIntoHeight = (base * height) ;
     return baseIntoHeight;
+}
+function checkButtonHandler() {
+    var side1 = Number(sides[0].value);
+    var side2 = Number(sides[1].value);
+
+    if(!side1 || !side2  ) {
+        outPut.innerText="input all the fields" 
+    }
+    else if (side1<1||side2<1) {
+        outPut.innerText="input value should not be -"
+    }
+    else {
+        calculateArea(sides.value)
+    }
+
 }
 
 
 
 function calculateArea() {
     const baseIntoHeight = calculateBaseIntoHeight(Number(sides[0].value), Number(sides[1].value));
-
-    outPut.innerText = "The Area of Triangle  " + baseIntoHeight
+    const calArea = 1/2 * baseIntoHeight;
+    outPut.innerText = "The Area of Triangle  " + calArea;
 
 
     //console.log(baseIntoHeight)//
@@ -20,4 +35,8 @@ function calculateArea() {
 }
 
 
-areaBtn.addEventListener("click", calculateArea);
+
+
+areaBtn.addEventListener("click", checkButtonHandler);
+
+//if ((angle1 !== "") && (angle2 !== "") && (angle3 !== "")) {
